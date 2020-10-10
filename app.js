8bb40b32a078368638a8cbba3976d56e88cced71
Rushil Sharma <srushil64@gmail.com>
2020-10-10 15:59:20 +0530

    test

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/:username/:platform/:leagueId', (req, res) => {
     });
     req.on('end', () => {
         const responseBody = { body };
-        console.log(body);
+        console.log(responseBody);
         /*res.write(JSON.stringify(JSON.parse(body)));
         res.end();*/
         //const { leagueTeamInfoList: teams } = JSON.parse(body);
