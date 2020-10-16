07e11affb4ac4edc6847672b074c6788f4220112
Rushil Sharma <srushil64@gmail.com>
2020-10-16 15:49:27 +0530

    done folder structure

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.post('/:groupid/:platform/:leagueId/*', (req, res) => {
             //fs.writeFileSync('madden/export.txt', newexport);
             fs.writeFileSync('export.txt', newexport);
         }
-        fs.writeFileSync('export.txt', newexport);
+        fs.writeFileSync(__dirname+'/export.txt', newexport);
         // ============================ Ends ============================
 
         fs.writeFileSync(`${path}/${pageName}.json`, body); // Create dynamic files as per folders
